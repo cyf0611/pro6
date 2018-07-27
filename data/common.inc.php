4ed59f4bc957ed2c9af1ac018d8ded440bb1a59e
cyf0611 <cao942789776@gmail.com>
2018-07-27 20:18:47 +0800

    init

---
+++ data/common.inc.php
@@ -0,0 +1,12 @@
+<?php
+//数据库连接信息
+$cfg_dbtype = 'mysql';
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'www_zhendasm_co';
+$cfg_dbuser = 'www_zhendasm_co';
+$cfg_dbpwd = 'i6Q4Tnxbn5fQThZC';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
